15d95a49449d3480d7003e9f6a4ab02e45626e0d
Cross <caseycross1025@gmail.com>
2024-04-02 01:40:13 -0500

    Setup MySQL docker container. built out model for recipe.

--- recipe-service/src/main/java/com/cross/recipe/event/UserRecipeEvent.java
+++ recipe-service/src/main/java/com/cross/recipe/event/UserRecipeEvent.java
@@ -1,15 +1,14 @@
 package com.cross.recipe.event;
 
 import jakarta.persistence.ElementCollection;
+import lombok.Data;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 import java.util.List;
 
-@Getter
-@Setter
-@RequiredArgsConstructor
+@Data
 public class UserRecipeEvent {
 
     long userId;
